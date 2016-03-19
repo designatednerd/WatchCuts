b367256c636d97a961693d919cd4c60f85af0cb8
Thomas Martin <tmartin@nerdery.com>
2016-03-19 17:32:00 -0500

    added cocoa pods and CorePlot. Basic plot slightly showing up.

---
+++ WatchCuts/WatchCuts/WatchCuts-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  WatchCuts-Bridging-Header.h
+//  WatchCuts
+//
+//  Created by Thomas Martin on 3/19/16.
+//  Copyright © 2016 Baseball Hack Day 2016. All rights reserved.
+//
+
+#ifndef WatchCuts_Bridging_Header_h
+#define WatchCuts_Bridging_Header_h
+
+#import "CorePlot-CocoaTouch.h"
+
+#endif /* WatchCuts_Bridging_Header_h */
