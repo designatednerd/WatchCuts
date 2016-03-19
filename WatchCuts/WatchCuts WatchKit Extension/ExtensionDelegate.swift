19b7e779c16a2b058da4340145c67927135f2727
Ellen Shapiro <designatednerd@gmail.com>
2016-03-19 17:21:48 -0500

    Work on using recorded data instead.

--- WatchCuts/WatchCuts WatchKit Extension/ExtensionDelegate.swift
+++ WatchCuts/WatchCuts WatchKit Extension/ExtensionDelegate.swift
@@ -12,7 +12,7 @@ import WatchConnectivity
 
 class ExtensionDelegate: NSObject, WKExtensionDelegate, WCSessionDelegate {
     
-    let motionManager = CMMotionManager()
+    let recorder = CMSensorRecorder()
     let motionQueue: NSOperationQueue = {
         let queue = NSOperationQueue()
         queue.qualityOfService = .UserInitiated
@@ -27,10 +27,15 @@ class ExtensionDelegate: NSObject, WKExtensionDelegate, WCSessionDelegate {
             }
         }
     }
+    var flushing = false
+    
     
     //MARK: Extension  Lifecycle
     func applicationDidFinishLaunching() {
         // Perform any final initialization of your application.
+        if CMSensorRecorder.isAccelerometerRecordingAvailable() {
+            self.recorder.recordAccelerometerForDuration(20 * 60)
+        }
     }
     
     func applicationDidBecomeActive() {
@@ -39,34 +44,40 @@ class ExtensionDelegate: NSObject, WKExtensionDelegate, WCSessionDelegate {
         
         self.session = WCSession.defaultSession()
         
+        let now = NSDate()
+        guard let dataList = self.recorder.accelerometerDataFromDate(now.dateByAddingTimeInterval(-60), toDate: now) else {
+            NSLog("NO DATA FOR YOU")
+            return
+        }
         
-        self.motionManager.accelerometerUpdateInterval = Double(1/60)
-        self.motionManager.startAccelerometerUpdatesToQueue(self.motionQueue, withHandler: self.accelerometerHandler())
+        
+        for (_, item) in dataList.enumerate() {
+            guard let data = item as? CMRecordedAccelerometerData else {
+                NSLog("NOT UR DATASZ")
+                return
+            }
+            
+            let accel = Acceleration(x: data.acceleration.x, y: data.acceleration.y, z: data.acceleration.z)
+            accelerations.append(accel)
+            print("\(accel)")
+        }
+        
+        
+        guard let lastAccel = self.accelerations.last else {
+            return
+        }
+        //Update UI
+        if let interfaceController = WKExtension.sharedExtension().rootInterfaceController as? InterfaceController {
+            interfaceController.updateUIWithAccelleration(lastAccel)
+        }
         
     }
     
     func applicationWillResignActive() {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, etc.
-        self.motionManager.stopAccelerometerUpdates()
         
-        if WCSession.isSupported() {
-            self.session?.sendMessage([WatchKitMessageKey.accels.rawValue: self.accelerations],
-                replyHandler: {
-                    [weak self]
-                    dictionary in
-                    
-                    if dictionary[WatchKitMessageKey.success.rawValue] as? Bool == true {
-                        self?.accelerations.removeAll()
-                        NSLog("RESET ACCELS")
-                    }
-                },
-                errorHandler: {
-                    error in
-                    NSLog("ERROR: \(error)")
-            })
-        }
-        
+        self.flushAccellerationsToPhone(self.accelerations)
         
         NSLog("WILL RESIGN ACTIVE")
     }
@@ -84,14 +95,70 @@ class ExtensionDelegate: NSObject, WKExtensionDelegate, WCSessionDelegate {
             if let data = accelerometerData {
                 let acceleration = data.acceleration
                 let fromCM = Acceleration.fromCMAcceleration( acceleration)
-                self?.accelerations.append(fromCM)
+                
+                guard let strongSelf = self else {
+                    NSLog("BAIL 1")
+                    return
+                }
+                
+                strongSelf.accelerations.append(fromCM)
+
                 NSOperationQueue.mainQueue().addOperationWithBlock {
                     //Update UI
                     if let interfaceController = WKExtension.sharedExtension().rootInterfaceController as? InterfaceController {
                         interfaceController.updateUIWithAccelleration(fromCM)
                     }
+                    
+                    guard let ss2 = self else {
+                        NSLog("BAIL 2!")
+                        return
+                    }
+                    
+                    if ss2.accelerations.count > 100 && !ss2.flushing {
+                        let first100 = ss2.accelerations.prefix(100)
+                        ss2.flushAccellerationsToPhone(Array(first100))
+                    }
                 }
             }
         }
     }
+    
+    func flushAccellerationsToPhone(flushMe: [Acceleration]) {
+//        if WCSession.isSupported() {
+//            NSLog("FLOOSH \(flushMe.count)")
+//            self.flushing = true
+//            let encoded = NSKeyedArchiver.archivedDataWithRootObject(flushMe)
+//            self.session?.sendMessage([WatchKitMessageKey.accels.rawValue: encoded],
+//                replyHandler: {
+//                    [weak self]
+//                    dictionary in
+//                    
+//                    if let strongSelf = self {
+//                        if let success = dictionary[WatchKitMessageKey.success.rawValue] as? NSNumber where success.boolValue == true {
+//                            strongSelf.accelerations = Array(strongSelf.accelerations.dropFirst(flushMe.count))
+//                            strongSelf.flushing = false
+//                            NSLog("RESET ACCELS")
+//                        } else {
+//                            NSLog("DINNAE GET RETURN")
+//                        }
+//                        
+//                    } else {
+//                        NSLog("SELFLESS")
+//                    }
+//                },
+//                errorHandler: {
+//                    error in
+//                    NSLog("ERROR: \(error)")
+//            })
+//        } else {
+//            NSLog("SESSION NOT SUPPORTED")
+//        }
+    }
 }
+
+extension CMSensorDataList: SequenceType {
+    public func generate() -> NSFastGenerator {
+        return NSFastGenerator(self)
+    }
+}
+
