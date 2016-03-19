de15e4caa2b99ff341655dfab2395102080dfad7
Ellen Shapiro <designatednerd@gmail.com>
2016-03-19 15:48:09 -0500

    re-enable reply data handling

--- WatchCuts/WatchCuts/AppDelegate.swift
+++ WatchCuts/WatchCuts/AppDelegate.swift
@@ -19,12 +19,15 @@ extension AppDelegate: WCSessionDelegate {
 
     func session(session: WCSession, didReceiveMessage message: [String : AnyObject], replyHandler: ([String : AnyObject]) -> Void) {
         
-        if let accels = message[WatchKitMessageKey.accels.rawValue] as? [Acceleration] {
+        if
+        let accelData = message[WatchKitMessageKey.accels.rawValue] as? NSData,
+        let accels = NSKeyedUnarchiver.unarchiveObjectWithData(accelData) as? [Acceleration]
+        {
             NSLog("Accelerations: \(accels)")
-//            replyHandler([WatchKitMessageKey.success.rawValue: NSNumber(bool: true)])
+            replyHandler([WatchKitMessageKey.success.rawValue: NSNumber(bool: true)])
         } else {
             NSLog("YOU CANNOT HAS")
-//            replyHandddler([WatchKitMessageKey.success.rawValue: NSNumber(bool: false)])
+            replyHandler([WatchKitMessageKey.success.rawValue: NSNumber(bool: false)])
         }
     }
 }
